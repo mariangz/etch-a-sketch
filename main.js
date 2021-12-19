206797d209c4424789f89639aac0e398999f75df
mariangz <marianosgimenez@gmail.com>
2021-12-19 02:08:49 +0100

    add functions

--- main.js
+++ main.js
@@ -1,29 +1,53 @@
+const $blackWhite = document.getElementById('blackWhite');
+const $rainbow = document.getElementById('rainbow');
+const $range = document.getElementById('range');
+const $grid = document.querySelector('.grid');
+const black = '#000';
+const white = '#fff';
+const random = () => Math.floor(Math.random() * 257);
+let choosedColor;
+
+const paint = (node) => {
+  node.forEach((box) => {
+    box.addEventListener(
+      'mouseover',
+      (e) =>
+        (e.target.style.backgroundColor =
+          choosedColor || `rgb(${random()}, ${random()}, ${random()})`)
+    );
+  });
+};
+
+document.addEventListener('DOMContentLoaded', create(16));
+$range.addEventListener('change', () => create($range.value));
+$rainbow.addEventListener('click', () => (choosedColor = undefined));
+$blackWhite.addEventListener('click', () => {
+  if (choosedColor != white) {
+    choosedColor = white;
+  } else {
+    choosedColor = black;
+  }
+});
+
 function create(n) {
-  const grid = document.querySelector('.grid');
+  remove();
   const r = document.querySelector(':root');
   const rs = getComputedStyle(r);
-
-  console.log(rs.getPropertyValue('--boxNumber'));
   r.style.setProperty('--boxNumber', `${n}`);
-
-  let div;
 
   for (let i = 0; i < n * n; i++) {
     div = document.createElement('div');
     div.classList.add('box');
-    grid.appendChild(div);
+    $grid.appendChild(div);
+  }
+  const $node = document.querySelectorAll('.box');
+  paint($node);
+}
+
+function remove() {
+  if ($grid.firstChild) {
+    while ($grid.firstChild) {
+      $grid.removeChild($grid.lastChild);
+    }
   }
 }
-create(36);
-
-const random = () => Math.floor(Math.random() * 257);
-
-const paint = (box) =>
-  console.log(
-    (box.target.style.backgroundColor = `rgba(${random()}, ${random()}, ${random()}`)
-  );
-
-const node = document.querySelectorAll('.box');
-
-node.forEach((box) => box.addEventListener('mouseover', (e) => paint(e)));
-// console.log(node);
